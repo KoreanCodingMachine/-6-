167ca4ff9982f6f7d3186d9645bc23330c539fec
KoreanCodingMachine <pon03078@naver.com>
2022-08-12 16:53:21 +0900

    서버,클라

---
+++ mini/README.md
@@ -0,0 +1,15 @@
+# 프로그램 실행 
+cd mini  
+yarn start
+
+# 패키지 설치 목록 
+리덕스 설치 : yarn add react-redux  
+툴킷 (리덕스) 설치 : yarn add @reduxjs/toolkitthunk   
+(미들웨어) 설치 : yarn add redux-thunk  
+axios(통신) 설치 : yarn add axios  
+라우터 (페이지 이동) 설치 : yarn add react-router-dom  
+logger (개발 편하게 도와줌) 설치 : yarn add redux-loger  
+styled-components (스타일 적용) 설치 : yarn add styled-components  
+유효성 검사(폼 사용자 입력 안내 적용) : yarn add react-hook-form  
+observer (무한스크롤) 설치 : yarn add react-intersection-observer  
+material ui 설치 : yarn add @material-ui/core
---
+++ mini/src/redux/store.js
@@ -0,0 +1,25 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import comments from './modules/commentsSlice';
+import comment from './modules/commentSlice';
+import thunk from 'redux-thunk';
+import user from './modules/userSlice';
+import postSlice from './modules/postSlice';
+const middlewares = [thunk];
+
+// 리듀서 통합
+const rootReducer = combineReducers({
+  postSlice,
+  comments,
+  comment,
+  user,
+  devTools: false, // 배포환경 리덕스 데브툴즈 안보이게 설정
+});
+
+// 스토어 연결
+const store = configureStore({
+  reducer: rootReducer,
+
+  middleware: [...middlewares],
+});
+
+export default store;
