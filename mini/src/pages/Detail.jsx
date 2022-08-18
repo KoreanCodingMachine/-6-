1b03fde8dd0dd7ce190dbcc6e896c08ea8d09a4f
KoreanCodingMachine <pon03078@naver.com>
2022-08-18 18:19:00 +0900

    123

--- mini/src/pages/Detail.jsx
+++ mini/src/pages/Detail.jsx
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import Comment from '../components/comments/Comment';
+import Comment from '../components/write/Comment';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import styled from 'styled-components';
-import { useDispatch } from 'react-redux';
 import { useNavigate, useLocation } from 'react-router-dom';
 
 const Detail = () => {
