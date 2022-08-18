1bd9e2c7b9936e8fbe1210e3e8ae6ba090a137e5
KoreanCodingMachine <pon03078@naver.com>
2022-08-18 18:15:14 +0900

    123

--- mini/src/pages/Home.jsx
+++ mini/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Header from '../components/main/Header';
 import Time from '../components/main/Time';
 import List from '../components/main/List';
